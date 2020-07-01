8e07feda7515ed413ea25626aa47e3bd5cbe550e
Narendra <narendra1994yadav@gmail.com>
2020-07-01 08:47:19 +0530

    First commit

--- app/src/main/java/com/app/motion/ui/callbacks/OnItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.app.motion.ui.callbacks;
-
-import android.view.View;
-
-public interface OnItemClickListener<Object> {
-    void onItemClick(View view, Object object);
-}
--- app/src/main/java/com/app/motion/ui/model/BaseModel.java
+++
@@ -1,6 +0,0 @@
-package com.app.motion.ui.model;
-
-import androidx.databinding.BaseObservable;
-
-public class BaseModel extends BaseObservable {
-}
